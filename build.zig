52cc9de62179a692e61a34ab6876e5cb123a8bb8
Muhammad Saheed <muhammad.saheed.iam@gmail.com>
2025-04-14 10:58:16 +0530

    build a simple client and daemon

--- build.zig
+++ build.zig
@@ -4,30 +4,45 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe_mod = b.createModule(.{
-        .root_source_file = b.path("src/main.zig"),
+    const daemon_mod = b.createModule(.{
+        .root_source_file = b.path("src/daemon.zig"),
         .target = target,
         .optimize = optimize,
     });
-    const exe = b.addExecutable(.{
+    const daemon = b.addExecutable(.{
+        .name = "sparrowd",
+        .root_module = daemon_mod,
+    });
+    daemon.linkLibC();
+    b.installArtifact(daemon);
+    const daemon_cmd = b.addRunArtifact(daemon);
+    daemon_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| daemon_cmd.addArgs(args);
+    const daemon_step = b.step("daemon", "Run the daemon");
+    daemon_step.dependOn(&daemon_cmd.step);
+
+    const sparrow_mod = b.createModule(.{
+        .root_source_file = b.path("src/sparrow.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    const sparrow = b.addExecutable(.{
         .name = "sparrow",
-        .root_module = exe_mod,
+        .root_module = sparrow_mod,
     });
-    b.installArtifact(exe);
+    sparrow.linkLibC();
+    b.installArtifact(sparrow);
+    const sparrow_cmd = b.addRunArtifact(sparrow);
+    sparrow_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| sparrow_cmd.addArgs(args);
+    const sparrow_step = b.step("sparrow", "Run sparrow");
+    sparrow_step.dependOn(&sparrow_cmd.step);
 
-    const run_cmd = b.addRunArtifact(exe);
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    if (b.args) |args| run_cmd.addArgs(args);
-
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-
-
-    const exe_unit_tests = b.addTest(.{
-        .root_module = exe_mod,
+    const daemon_unit_tests = b.addTest(.{
+        .root_module = daemon_mod,
     });
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+
+    const run_unit_tests = b.addRunArtifact(daemon_unit_tests);
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_exe_unit_tests.step);
+    test_step.dependOn(&run_unit_tests.step);
 }
---
+++ src/sparrow.zig
@@ -0,0 +1,152 @@
+const std = @import("std");
+const lib = @import("sparrow_lib");
+
+const input = @cImport({
+    @cInclude("linux/input.h");
+});
+
+fn syncronize(fd: std.posix.fd_t) void {
+    _ = std.posix.system.write(
+        fd,
+        @ptrCast(&input.input_event{
+            .type = input.EV_SYN,
+            .code = input.SYN_REPORT,
+            .value = 0,
+        }),
+        @sizeOf(input.input_event),
+    );
+}
+
+fn mouse_click(fd: std.posix.fd_t, button: enum(u16) {
+    left = input.BTN_LEFT,
+    right = input.BTN_RIGHT,
+    middle = input.BTN_MIDDLE,
+}) void {
+    _ = std.posix.system.write(
+        fd,
+        @ptrCast(&input.input_event{
+            .type = input.EV_KEY,
+            .code = @intFromEnum(button),
+            .value = 1,
+        }),
+        @sizeOf(input.input_event),
+    );
+
+    _ = std.posix.system.write(
+        fd,
+        @ptrCast(&input.input_event{
+            .type = input.EV_KEY,
+            .code = @intFromEnum(button),
+            .value = 0,
+        }),
+        @sizeOf(input.input_event),
+    );
+}
+
+fn move_mouse(fd: std.posix.fd_t, delta: struct {
+    x: i32,
+    y: i32,
+}) void {
+    _ = std.posix.system.write(
+        fd,
+        @ptrCast(&input.input_event{
+            .type = input.EV_REL,
+            .code = input.REL_X,
+            .value = delta.x,
+        }),
+        @sizeOf(input.input_event),
+    );
+
+    _ = std.posix.system.write(
+        fd,
+        @ptrCast(&input.input_event{
+            .type = input.EV_REL,
+            .code = input.REL_Y,
+            .value = delta.y,
+        }),
+        @sizeOf(input.input_event),
+    );
+}
+
+pub fn main() !void {
+    const args = std.os.argv;
+    if (args.len == 1)
+        return error.NO_INPUT_DEVICE_PROVIDED;
+    if (args.len > 2)
+        return error.INVALID_ARGS;
+
+    const device_name: []u8 = std.mem.span(args[1]);
+    const device = try std.fs.openFileAbsolute(device_name, .{
+        .mode = .read_only,
+    });
+    defer device.close();
+    std.debug.print("Listening on {s}...\n", .{device_name});
+
+    const daemon_socket = try std.posix.socket(
+        std.posix.AF.UNIX,
+        std.posix.SOCK.DGRAM,
+        0,
+    );
+
+    const env_xrd = std.posix.getenv("XDG_RUNTIME_DIR");
+    const socket_path = try std.fmt.bufPrint(
+        blk: {
+            var buf: [108]u8 = undefined;
+            break :blk &buf;
+        },
+        "{s}/.sparrow_socket",
+        .{env_xrd orelse "/tmp"},
+    );
+
+    const address = try std.net.Address.initUnix(socket_path);
+    try std.posix.connect(
+        daemon_socket,
+        &address.any,
+        address.getOsSockLen(),
+    );
+
+    var event = input.input_event{};
+    const event_size = @sizeOf(input.input_event);
+    while (true) {
+        if (std.posix.system.read(
+            device.handle,
+            @ptrCast(&event),
+            event_size,
+        ) == event_size) {
+            if (event.type == input.EV_KEY and
+                (event.value == 2 or event.value == 1))
+            {
+                switch (event.code) {
+                    input.KEY_H => {
+                        move_mouse(daemon_socket, .{ .x = -10, .y = 0 });
+                    },
+                    input.KEY_J => {
+                        move_mouse(daemon_socket, .{ .x = 0, .y = 10 });
+                    },
+                    input.KEY_K => {
+                        move_mouse(daemon_socket, .{ .x = 0, .y = -10 });
+                    },
+                    input.KEY_L => {
+                        move_mouse(daemon_socket, .{ .x = 10, .y = 0 });
+                    },
+                    input.KEY_M => {
+                        mouse_click(daemon_socket, .left);
+                    },
+                    input.KEY_COMMA => {
+                        mouse_click(daemon_socket, .middle);
+                    },
+                    input.KEY_DOT => {
+                        mouse_click(daemon_socket, .right);
+                    },
+                    else => {
+                        std.debug.print("Key code {d} {d}\n", .{
+                            event.code,
+                            event.value,
+                        });
+                    },
+                }
+                syncronize(device.handle);
+            }
+        }
+    }
+}
